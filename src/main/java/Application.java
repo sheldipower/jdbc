504d44925e2fcf4a44f590eeb233470df2aec490
Irina <sheldipower@gmail.com>
2023-05-21 14:48:13 +0500

    Hibernate 1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,7 @@
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.sql.*;
 import java.util.List;
 import java.util.Scanner;
@@ -10,14 +14,18 @@ public class Application {
         employeeDao.createEmployee(new Employee("Денис", "Маляров", "Man", 40, 3));
 
         int runSoft = 0;
-        while (runSoft != 5) {
+        while (runSoft != 6) {
             printMenu();
             switch (runSoft = scanner.nextInt()) {
                 case 1:
                     System.out.println("Введите id интересующего сотрудника");
                     int id = scanner.nextInt();
-                    Employee employee = employeeDao.getEmployeeById(id);
-                    System.out.println(employee);
+                    Employee employee1 = employeeDao.getEmployeeById(id);
+                    if (employee1 == null) {
+                        System.out.println("Такого сотрудника не существует");
+                    } else {
+                        System.out.println(employee1);
+                    }
                     break;
                 case 2:
                     List<Employee> employees = employeeDao.getAllEmployees();
@@ -28,15 +36,46 @@ public class Application {
                 case 3:
                     System.out.println("Введите id сотрудника для редактирования");
                     id = scanner.nextInt();
-                    employeeDao.changeEmployeeById(id);
+                    Employee employee2 = employeeDao.getEmployeeById(id);
+                    System.out.println("Введите новые данные сотрудника:");
+                    System.out.println("Имя");
+                    String first_name = scanner.nextLine();
+                    System.out.println("Фамилия");
+                    String last_name = scanner.nextLine();
+                    System.out.println("Пол");
+                    String gender = scanner.nextLine();
+                    System.out.println("Возраст");
+                    int age = scanner.nextInt();
+                    System.out.println("id города");
+                    int cityId = scanner.nextInt();
+                    Employee employee3 = new Employee(first_name, last_name, gender, age, cityId);
+                    employee3.setId(employee2.getId());
+                    employeeDao.updateEmployeeById(employee3);
                     break;
                 case 4:
                     System.out.println("Введите id сотрудника которого хотите удалить");
                     id = scanner.nextInt();
-                    employeeDao.dropEmployeeById(id);
+                    Employee employee4 = employeeDao.getEmployeeById(id);
+                    employeeDao.deleteEmployeeById(employee4);
+                    HibernateManager.closeEntityManagerFactory();
                     break;
                 case 5:
+                    System.out.println("Введите данные нового сотрудника:");
+                    System.out.println("Имя");
+                    String first_name1 = scanner.nextLine();
+                    System.out.println("Фамилия");
+                    String last_name1 = scanner.nextLine();
+                    System.out.println("Пол");
+                    String gender1 = scanner.nextLine();
+                    System.out.println("Возраст");
+                    int age1 = scanner.nextInt();
+                    System.out.println("id города");
+                    int cityId1 = scanner.nextInt();
+                    Employee employeeFromScanner = new Employee(first_name1, last_name1, gender1, age1, cityId1);
+
+                    employeeDao.createEmployee(employeeFromScanner);
                     break;
+                case 6:
                 default:
                     System.out.println("Данный вариант отсутствует");
                     break;
@@ -50,6 +89,7 @@ public class Application {
                 "2 - Получить список всех сотрудников \n " +
                 "3 - Изменить данные сотрудника \n " +
                 "4 - Удалить сотрудника из базы \n " +
-                "5 - Выход");
+                "5 - Добавить сотрудника \n " +
+                "6 - Выход");
     }
 }
